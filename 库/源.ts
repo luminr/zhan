c149ac6a54c6a6ecb77cbbdc890680503fe1ff9d
宇宙 <luminr@users.noreply.github.com>
2023-12-16 20:46:37 +0900

    长此以往

--- 库/源.ts
+++ 库/源.ts
@@ -1,5 +1,8 @@
 import { DOMParser } from "npm:linkedom@0.15.4";
 export { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
-export { dynamicImport, importString } from "https://deno.land/x/import@0.2.1/mod.ts";
+export {
+	dynamicImport,
+	importString,
+} from "https://raw.githubusercontent.com/luminr/import/411867570bbc4671b00e11576a0503d19c52e4a3/mod.ts";
 // @ts-ignore polyfill
 globalThis.DOMParser = DOMParser;
